70af60c967c96233b34a0473e56fa415ffdb0a59
Keith Davis <keithdavis@pridedallas.com>
2020-04-01 11:53:26 -0500

    116350 - Development (General) - Chocolatey Selenium WebDrivers - Review - 03/24/2020 - Updated choco install selenium-chromium-edge-driver to 81.0.416.41

--- selenium-chromium-edge-driver/x64/tools/chocolateyinstall.ps1
+++ selenium-chromium-edge-driver/x64/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName   = 'selenium-chromium-edge-driver'
-  url           = 'https://msedgedriver.azureedge.net/81.0.363.0/edgedriver_win64.zip'
-  checksum      = '2f1850a8bd4492f3a2af648dd8ae8ec6670a76505bfbe850f50ebb16186a32e4785ae5fc4686ebdb0be78fce3aa2f00c1bb6ca7d453a369f1768a8c15b213af3'
+  url           = 'https://msedgedriver.azureedge.net/81.0.416.41/edgedriver_win64.zip'
+  checksum      = 'B81499F5FCCA0536C36F34C8C80F6B78EED85B1D5212D41CBB2CB407D4A499218859513E6E4412559D5EC8067A3448B8FBF362DBFBABB71726C2CCB066C97239'
   checksumType  = 'sha512'
   unzipLocation = $seleniumDir
 }
